230309df2b757ca18bbfb5469e7e57185a977590
K. Kamitsukasa <jou4.kz@gmail.com>
2011-09-15 16:56:59 +0900

    Use util.s for fizzbuzz.

--- fizzbuzz.s
+++ fizzbuzz.s
@@ -1,10 +1,16 @@
 ; nasm -f macho64 fizzbuzz.s
-; gcc -o fizzbuzz fizzbuzz.o
+; nasm -f macho64 util.s
+; gcc -o fizzbuzz fizzbuzz.o util.o
 ; ./fizzbuzz
 ; ./fizzbuzz 100
 
 extern _printf
 extern _atoi
+
+extern print
+extern println
+extern puts
+extern exit
 
 section .data
     br              db      0x0a, 0
@@ -32,7 +38,7 @@ _handle_arg_begin:
     cmp rax, 1
     jle _handle_args_end
     mov rax, [rbp - 16]
-    mov rdi, [rax + 8]              ; param
+    mov rdi, [rax + 8]              ; argv[1]
     call _atoi
     mov [rbp - 24], rax
 _handle_args_end:
@@ -166,89 +172,3 @@ print_number_4:
     pop rbp
     ret
 
-
-strlen:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
-    mov qword [rbp - 8], 0
-strlen_cmp:
-    cmp byte [rax], 0
-    je strlen_end
-    inc rax
-    inc qword [rbp - 8]
-    jmp strlen_cmp
-strlen_end:
-    mov rax, [rbp - 8]  ; return
-    mov rsp, rbp
-    pop rbp
-    ret
-
-
-print:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 32
-print_1:
-    ; get length of string
-    mov rax, [rbp + 16]
-    mov [rsp], rax
-    call strlen
-    mov [rbp - 8], rax
-    ; print string
-    mov rax, [rbp + 16]
-    mov [rsp], rax
-    mov rax, [rbp - 8]
-    mov [rsp + 8], rax
-    call puts
-print_2:
-    mov rsp, rbp
-    pop rbp
-    ret
-
-println:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
-println_1:
-    ; print string
-    mov rax, [rbp + 16]
-    mov [rsp], rax
-    call print
-    ; print breakline
-    mov rax, br
-    mov [rsp], rax
-    call print
-println_2:
-    mov rsp, rbp
-    pop rbp
-    ret
-
-
-puts:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
-puts_1:
-    mov [rbp - 8], rdi
-    mov [rbp - 16], rsi
-puts_2:
-    ; print string
-    mov rax, 0x2000004      ; System call write = 4
-    mov rdi, 1              ; Write to standard out = 1
-    mov rsi, [rbp + 16]     ; The address of string
-    mov rdx, [rbp + 24]     ; The size to write
-    syscall                 ; Invoke the kernel
-puts_3:
-    mov rdi, [rbp - 8]
-    mov rsi, [rbp - 16]
-puts_4:
-    mov rsp, rbp
-    pop rbp
-    ret
-
-
-exit:
-    mov rax, 0x2000001      ; System call number for exit = 1
-    mov rdi, 0              ; Exit success = 0
-    syscall                 ; Invoke the kernel
